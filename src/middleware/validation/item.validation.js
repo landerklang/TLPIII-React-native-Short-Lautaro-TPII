a70b945060022256777ee7dd2873b68dd5a76ef0
¨lautaro <shortlauty35@gmail.com>
2026-04-04 09:49:07 -0300

    se realizo las validaciones de los items

--- src/middleware/validation/item.validation.js
+++ src/middleware/validation/item.validation.js
@@ -17,4 +17,32 @@ export const createdItemValidation = [
         throw new Error("ya existe un objeto con ese nombre");
       }
     }),
+  body("Icon")
+    .isString()
+    .withMessage("la direcion del objeto debe de ser string")
+    .optional(),
+  body("Typeitem")
+    .notEmpty()
+    .withMessage(
+      "es obligatorio la especificacion del tipo de objeto si es Passive o Active",
+    )
+    .isIn(["Passive", "Active"])
+    .withMessage("solamente se permite objetos Active o Passive"),
+  body("Quote")
+    .notEmpty()
+    .withMessage("es obligatorio el ingresar la cita textualmente"),
+  body("Description")
+    .notEmpty()
+    .withMessage("es obligatorio el escribir la descripcion del objeto "),
+  body("Quality")
+    .notEmpty()
+    .withMessage(
+      "se debe ingresar el crado que debe ser este objeto siendo del 0 al 4, 4 es el mas raro",
+    )
+    .isInt()
+    .withMessage(
+      "solamente se permite numero en este apartado los numero son del 0 al 4 indicando la raresa del objetos desde la mas comun a la mas rarar",
+    )
+    .isIn([0, 1, 2, 3, 4])
+    .withMessage("solamente se permiten los grados 0,1,2,3,4"),
 ];
